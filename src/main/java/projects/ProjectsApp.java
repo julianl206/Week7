49c862c4a196c4dce53c1e62fbdbe78391cfc8db
Julian Luna <julianlunavas@gmail.com>
2023-02-17 23:16:39 -0800

    finished hw

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,29 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		DbConnection.getConnection();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,49 @@
+package projects.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import projects.exception.DbException;
+public class DbConnection {
+
+	public DbConnection() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static String HOST = "localhost";
+	private static String PASSWORD = "Ilovenicho@12";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", HOST, PORT, SCHEMA, USER, PASSWORD);
+		System.out.println("Connectiong with url=" + url);
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Successfully obtained connection!");
+			return conn;
+		} catch (SQLException e) {
+			throw new DbException(e);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		}
+	}
+	
+}
